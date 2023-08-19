2c05c5a1786384ae9509f2902bfc639f290fed82
jiminKim <100477188+jimin-0415@users.noreply.github.com>
2023-08-19 10:12:38 +0900

    Feat: Camera and Spring Arm
    - Add Camera Component
    - Add SpringArm Componet

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "BlasterCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 
 // Sets default values
 ABlasterCharacter::ABlasterCharacter()
@@ -9,6 +12,16 @@ ABlasterCharacter::ABlasterCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Spring Arm
+	m_CameraBoom = CreateDefaultSubobject< USpringArmComponent>( TEXT( "CameraBoom" ) );
+	m_CameraBoom->SetupAttachment( GetMesh() );
+	m_CameraBoom->TargetArmLength = 600.f;
+	m_CameraBoom->bUsePawnControlRotation = true;
+	
+	//Follow Camera
+	m_FollowCamera = CreateDefaultSubobject<UCameraComponent>( TEXT( "FollowCamera" ) );
+	m_FollowCamera->SetupAttachment( m_CameraBoom, USpringArmComponent::SocketName );
+	m_FollowCamera->bUsePawnControlRotation = false;
 }
 
 // Called when the game starts or when spawned
--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -18,15 +18,22 @@ public:
 	// Sets default values for this character's properties
 	ABlasterCharacter();
 
+	// Called every frame
+	virtual void Tick( float DeltaTime ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent( class UInputComponent* PlayerInputComponent ) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	/// Spring Arm Component
+	UPROPERTY( VisibleAnyWhere, Category = Camera )
+	class USpringArmComponent* m_CameraBoom;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	/// Camera Component
+	UPROPERTY( VisibleAnyWhere, Category = Camera )
+	class UCameraComponent* m_FollowCamera;
 };
